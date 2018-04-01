fac807054d70091050e5ba97ffb985679038220c
shubo7996 <whatsupmofo420@gmail.com>
2018-04-01 14:42:39 +0530

    updating svm

--- svm_from_scratch.py
+++ svm_from_scratch.py
@@ -63,7 +63,47 @@ class Support_Vector_Machine:
 	def predict(self, features):
 		#sign x.w+b
 		classification = np.sign(np.dot(np.array(features),self.w)+self.b)
+		if classification != 0 and self.visualization:
+			self.ax.scatter(features[0],features[1], s=200, marker=*,c= self.colors[classification])
 		return classification
+
+	def visualization(self):
+		[[self.ax.scatter(x[0],x[1],s=100,color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+		def hyperplane(x,w,b,v):
+			return (-w[0]*x-b+v / w[1])
+
+		datarange = (self.min_feature_value*0.9,self.max_feature*1.1)
+		hyp_x_min = datarange[0]
+		hyp_x_max = datarange[1]
+
+		#(w.x+b)=1
+		#positive support vector hyperplane
+		psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+		psv2 = hyperplane(hyp_x_max,self.w,self.b,1)
+		self.ax.plot([hyp_x_min,hyp_x_max],[psv1,psv2])
+
+		#(w.x+b)=-1
+		#negetive support vector hyperplane
+		psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+		psv2 = hyperplane(hyp_x_max,self.w,self.b,1)
+		self.ax.plot([hyp_x_min,hyp_x_max],[psv1,psv2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #adding data to the class
